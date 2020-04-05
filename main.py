e664625bd61195f2036bfe416c45200ab330021a
mkalino1 <mateusz.kalinowski1998@gmail.com>
2020-04-05 18:56:32 +0200

    Adding proper method functions

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def hello_pandemic():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+
+@app.get("/method")
+def read_item():
+    return {"method": "GET"}
+
+
+@app.post("/method")
+def read_item():
+    return {"method": "POST"}
+
+
+@app.put("/method")
+def read_item():
+    return {"method": "PUT"}
+
+
+@app.delete("/method")
+def read_item():
+    return {"method": "DELETE"}
